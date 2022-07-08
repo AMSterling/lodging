630280a1fc240a7cc24416ba35d11eb3144e9561
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-07 18:19:53 -0600

    Rooms class tests passing

---
+++ app/controllers/hotel_rooms_controller.rb
@@ -0,0 +1,5 @@
+class HotelRoomsController < ApplicationController
+  def index
+    
+  end
+end
--- spec/features/rooms/rooms_spec.rb
+++ spec/features/rooms/rooms_spec.rb
@@ -9,7 +9,23 @@ RSpec.describe 'rooms tables' do
     standard = marriott.rooms.create!(name: 'Standard', occupied: false, guest_count: 0)
     deluxe = marriott.rooms.create!(name: 'Deluxe', occupied: true, guest_count: 2)
 
-    visit "/hotels/#{marriott.id}/rooms"
+    visit '/rooms'
+
+    expect(page).to have_content(standard.name)
+    expect(page).to have_content(standard.occupied)
+    expect(page).to have_content(standard.guest_count)
+  end
+
+# As a visitor
+# When I visit '/child_table_name/:id'
+# Then I see the child with that id including the child's attributes:
+
+  it 'shows the room with that ID with its attributes' do
+    marriott = Hotel.create!(name: 'Marriott', city: 'Jacksonville', booked: false, capacity: 400)
+    standard = marriott.rooms.create!(name: 'Standard', occupied: false, guest_count: 0)
+    deluxe = marriott.rooms.create!(name: 'Deluxe', occupied: true, guest_count: 2)
+
+    visit "/rooms/#{standard.id}"
 
     expect(page).to have_content(standard.name)
     expect(page).to have_content(standard.occupied)
