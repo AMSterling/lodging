398d00496e30a7c2562561c8eca7a0b283f6f886
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-10 19:07:10 -0600

    Add update functionality

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,7 +3,22 @@ class RoomsController < ApplicationController
     @rooms = Room.all
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    room = Room.find(params[:id])
+    room.update(room_params)
+    redirect_to "/rooms/#{room.id}"
+  end
+
   def show
     @room = Room.find(params[:id])
   end
+end
+
+private
+def room_params
+  params.permit(:name, :occupied, :guest_count)
 end
