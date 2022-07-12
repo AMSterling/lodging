d72c7a4a9c9597beb91fa9057faea87172a8ff6d
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-11 20:12:52 -0600

    Add delete room functionality

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,6 +13,12 @@ class RoomsController < ApplicationController
     redirect_to "/rooms/#{room.id}"
   end
 
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to "/rooms"
+  end
+
   def show
     @room = Room.find(params[:id])
   end
