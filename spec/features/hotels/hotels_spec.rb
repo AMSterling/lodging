11ea9b84bf0c8e713f34097e5da76e7d5232adb0
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-12 17:27:35 -0600

    Add delete button for each hotel from hotel index/method test for filter

--- spec/features/hotels/hotels_spec.rb
+++ spec/features/hotels/hotels_spec.rb
@@ -107,6 +107,7 @@ RSpec.describe 'hotel table' do
     executive = ritz.rooms.create!(name: 'Executive', occupied: false, guest_count: 0)
 
     visit "/hotels/#{marriott.id}"
+
     click_on 'Hotel Index'
 
     expect(current_path).to eq("/hotels")
@@ -142,6 +143,7 @@ RSpec.describe 'hotel table' do
     executive = ritz.rooms.create!(name: 'Executive', occupied: false, guest_count: 0)
 
     visit "/hotels/#{marriott.id}"
+
     click_on 'Room Index'
 
     expect(current_path).to eq("/rooms")
@@ -208,6 +210,27 @@ RSpec.describe 'hotel table' do
       expect(page).to have_button("Edit #{marriott.name}")
       expect(page).to have_button("Edit #{hyatt.name}")
       expect(page).to have_button("Edit #{ritz.name}")
+    end
+  end
+
+  describe 'delete hotel from index' do
+    it 'has a link next to each hotel in the index to delete the hotel' do
+      marriott = Hotel.create!(name: 'Marriott', city: 'Jacksonville', booked: false, capacity: 400)
+      budget = marriott.rooms.create!(name: 'Budget', occupied: true, guest_count: 1)
+      standard = marriott.rooms.create!(name: 'Standard', occupied: false, guest_count: 0)
+      double = marriott.rooms.create!(name: 'Double', occupied: true, guest_count: 2)
+      hyatt = Hotel.create!(name: 'Hyatt', city: 'Denver', booked: false, capacity: 480)
+      king = hyatt.rooms.create!(name: 'King', occupied: true, guest_count: 1)
+      suite = hyatt.rooms.create!(name: 'Suite', occupied: true, guest_count: 4)
+      ritz = Hotel.create!(name: 'Ritz Carlton', city: 'Cleveland', booked: false, capacity: 600)
+      deluxe = ritz.rooms.create!(name: 'Deluxe', occupied: true, guest_count: 3)
+      executive = ritz.rooms.create!(name: 'Executive', occupied: false, guest_count: 0)
+
+      visit '/hotels'
+      
+      expect(page).to have_button("Delete #{marriott.name}")
+      expect(page).to have_button("Delete #{hyatt.name}")
+      expect(page).to have_button("Delete #{ritz.name}")
     end
   end
 end
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -43,6 +43,25 @@ RSpec.describe Hotel, type: :model do
           expect(ritz.room_count).to eq 2
         end
       end
+
+      describe 'rooms_with_more_than' do
+        it 'filters rooms where guest count is greater than user provided number' do
+          marriott = Hotel.create!(name: 'Marriott', city: 'Jacksonville', booked: false, capacity: 400)
+          budget = marriott.rooms.create!(name: 'Budget', occupied: true, guest_count: 1)
+          standard = marriott.rooms.create!(name: 'Standard', occupied: false, guest_count: 0)
+          double = marriott.rooms.create!(name: 'Double', occupied: true, guest_count: 2)
+          hyatt = Hotel.create!(name: 'Hyatt', city: 'Denver', booked: false, capacity: 480)
+          king = hyatt.rooms.create!(name: 'King', occupied: true, guest_count: 1)
+          suite = hyatt.rooms.create!(name: 'Suite', occupied: true, guest_count: 4)
+          ritz = Hotel.create!(name: 'Ritz Carlton', city: 'Cleveland', booked: false, capacity: 600)
+          deluxe = ritz.rooms.create!(name: 'Deluxe', occupied: true, guest_count: 3)
+          executive = ritz.rooms.create!(name: 'Executive', occupied: false, guest_count: 0)
+
+          expect(marriott.rooms_with_more_than(2).count).to eq 0
+          expect(hyatt.rooms_with_more_than(2)).to eq([suite])
+          expect(ritz.rooms_with_more_than(2)).to eq([deluxe])
+        end
+      end
     end
   end
 end
